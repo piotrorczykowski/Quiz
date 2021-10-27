70d624d8c73c31deead06b040f01377828e8c590
Piotr Orczykowski <orczykowski.peter@gmail.com>
2021-10-27 21:22:40 +0200

    Added function to display progress of quiz

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,8 @@ $(document).ready(function()
     const second_answer = $('#description-b');
     const third_answer = $('#description-c');
     const fourth_answer = $('#description-d');
+    const which_question = $('#which-question');
+    const max_question = $('#max-question');
     let index = 1;
     let score = 0;
 
@@ -17,23 +19,31 @@ $(document).ready(function()
         fourth_answer.text(data.answers[3]);
     }
 
-    const checkAnswer = (correctAnswer, userAnswer) => {(userAnswer==correctAnswer) ? score+=1 : score+=0;}
+    const updateProgress = (maxOfQuestion) => { which_question.text(index); max_question.text(maxOfQuestion); }
+    const checkAnswer = (correctAnswer, userAnswer) => { (userAnswer==correctAnswer) ? score+=1 : score+=0; }
 
     $.getJSON('questions.json', data => 
     {
         const number_of_questions = Object.keys(data).length;
         updateInformation(data[0]);
+        updateProgress(number_of_questions);
 
         $('button').click(event =>
         {
             if(index <= number_of_questions)
             {
                 checkAnswer(data[index-1].correct_answer_index, event.target.id);
+                console.log(index);
                 console.log(score);
 
                 if(index < number_of_questions)
+                {
                     updateInformation(data[index]);
-                index++;
+                    index++;
+                }
+                else index+=2;
+                
+                updateProgress(number_of_questions);
             }
         });
     });
